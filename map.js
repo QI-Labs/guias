e64f5dde6d6773cea03fb714963d409ebd1cc565
mimimalher <michellejorge17@gmail.com>
2014-05-24 18:54:08 -0300

    tirei o "text"

--- map.js
+++ map.js
@@ -3,14 +3,14 @@ module.exports =  {
 	vestibular: {
 		name: "Vestibular",
 		color: "red",
-		file: "text/vestibular/main.md",
+		file: "vestibular/main.md",
 		children: {
 			"exames-unificados": {
 				name: "Exames Unificados",
-				file: "text/vestibular/exames-unificados.md",
+				file: "vestibular/exames-unificados.md",
 				children: {
 					"enem": {
-						file: "text/vestibular/exames-unificados-enem.md",
+						file: "vestibular/exames-unificados-enem.md",
 					}
 				}
 			}

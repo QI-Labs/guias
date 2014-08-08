f00a950e60d74711fbeb086746243c86d717ee81
f03lipe <pires.a.felipe@gmail.com>
2014-08-07 21:31:09 -0300

    contributors field added to map.js

--- map.js
+++ map.js
@@ -128,6 +128,7 @@ module.exports =  {
 		name: "Olimpíadas de Matemática",
 		icon: 'icon-circle-compass',
 		file: "olimpiadas/matematica/main.md",
+		contributors: ['5320fd8ab6172102003081b9', '5321477b2816f6020050cb90', '5397b21e5c86f602009aa558'],
 		children: {
 			"introducao": {
 				name: "Introdução a Olimpíadas de Matemática",
@@ -220,4 +221,129 @@ module.exports =  {
 			}
 		}
 	},
+	vestibular: {
+		name: "Vestibular",
+		file: "vestibular/main.md",
+		children: {
+			"exames-unificados": {
+				name: "Exames Unificados",
+				file: "vestibular/exames-unificados.md",
+				children: {
+					"enem": {
+						name: "ENEM",
+						file: "vestibular/exames-unificados.enem.md",
+					},
+					"fuvest": {
+						name: "FUVEST",
+						file: "vestibular/exames-unificados.fuvest.md",
+					},
+					"unicamp": {
+						name: "UNICAMP",
+						file: "vestibular/exames-unificados.unicamp.md",
+					}
+				}
+			},
+			"preparacao-para-provas": {
+				name: "Preparação Para As Provas",
+				file: "vestibular/preparacao-para-provas.md",
+				children: {
+					"cursinhos": {
+						name: "Cursinhos",
+						file: "vestibular/preparacao-para-provas.cursinhos.md",
+					},
+					"estudando-em-casa": {
+						name: "Estudando em Casa",
+						file: "vestibular/preparacao-para-provas.estudando-casa.md",
+					},
+					"provas-antigas": {
+						name: "Provas Antigas & Conteúdo Cobrado",
+						file: "vestibular/preparacao-para-provas.provas-antigas.md",
+					},
+					"sites-uteis": {
+						name: "Sites Úteis",
+						file: "vestibular/preparacao-para-provas.sites-uteis.md",
+					}
+				}
+			},
+			"cronograma": {
+				name: "Cronograma",
+				file: "vestibular/cronograma.md",
+				children: {
+					"durante-o-ano": {
+						name: "Durante o Ano",
+						file: "vestibular/cronograma.durante-o-ano.md",
+					},
+					"semana-do-exame": {
+						name: "Na Semana do Exame",
+						file: "vestibular/cronograma.semana-do-exame.md",
+					},
+					"dia-anterior": {
+						name: "No Dia Anterior",
+						file: "vestibular/cronograma.dia-anterior.md",
+					},
+					"dia-do-exame": {
+						name: "No Dia do Exame",
+						file: "vestibular/cronograma.dia-do-exame.md",
+					}
+				}
+			},
+			"resultados-e-matricula": {
+				name: "Resultados e Matrícula",
+				file: "vestibular/resultados-e-matricula.md",
+				children: {
+					"classificacao-e-reclassificacao": {
+						name: "Classificação e Reclassificação",
+						file: "vestibular/resultados-e-matricula.classificacao-e-reclassificacao.md",
+					},
+					"matricula": {
+						name: "Matrícula",
+						file: "vestibular/resultados-e-matricula.matricula.md",
+					},
+					"cotas": {
+						name: "Cotas",
+						file: "vestibular/resultados-e-matricula.cotas.md",
+					},
+					"sisu": {
+						name: "SISU",
+						file: "vestibular/resultados-e-matricula.sisu.md",
+					},
+					"prouni": {
+						name: "Prouni",
+						file: "vestibular/resultados-e-matricula.prouni.md",
+					},
+					"fies": {
+						name: "Fies",
+						file: "vestibular/resultados-e-matricula.fies.md",
+					}
+				}
+			}
+		}
+	},
+	olimpiadas: {
+		name: "Olimpíadas Científicas",
+		file: "olimpiadas/main.md",
+		children: {
+			"olimpiadas-matematica": {
+				name: "Olimpíadas de Matemática",
+				redirect: "olimpiadas-matematica",
+			},
+			"olimpiadas-fisica": {
+				name: "Olimpíadas de Física",
+				redirect: "olimpiadas-fisica",
+			},
+			"olimpiadas-quimica": {
+				name: "Olimpíadas de Química",
+				redirect: "olimpiadas-quimica",
+			},
+			"olimpiadas-informatica": {
+				name: "Olimpíadas de Informática",
+				redirect: "olimpiadas-informática",
+			},
+			"olimpiadas-biologia": {
+				name: "Olimpíadas de Biologia",
+				redirect: "olimpiadas-biologia",
+			},
+		}
+	},
+
 }
